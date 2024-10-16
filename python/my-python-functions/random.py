f8b2807753a39639f75da5ff80c2bb9c84bc245c
produxman <mail@produxman.me>
2024-10-17 02:09:43 +0300

    main

--- python/my-python-functions/random.py
+++ python/my-python-functions/random.py
@@ -6,3 +6,17 @@ def shoppingCart():
     print(f"You have bought {quantity} x {item}/s for a total of {total}")
 
 shoppingCart()
+
+# A. donuts
+# Given an int count of a number of donuts, return a string
+# of the form 'Number of donuts: <count>', where <count> is the number
+# passed in. However, if the count is 10 or more, then use the word 'many'
+# instead of the actual count.
+# So donuts(5) returns 'Number of donuts: 5'
+# and donuts(23) returns 'Number of donuts: many'
+def donuts(count):
+  if count >= 10:
+    response = "Number of donuts: many"
+  else:
+    response = "Number of donuts: " + str(count)
+  return response
