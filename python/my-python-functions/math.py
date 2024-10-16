e30e93b648858aeafd80a1663b3479fd2ec91545
produxman <mail@produxman.me>
2024-10-17 01:46:03 +0300

    main

---
+++ python/my-python-functions/math.py
@@ -0,0 +1,28 @@
+def factorial(num):
+    if type(num) is int and num > 0:
+        i = 0
+        f = 1
+        while i < num:
+            i = i + 1
+            f = f * i
+        return f
+    else:
+        return None
+
+
+def hexToDec(hexNum):
+    hexNumbers = {
+    '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+    'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15
+}   
+    decNum = 0
+    exponent = len(hexNum)-1
+    for char in hexNum:
+        if char not in hexNumbers:
+            return 'wtf?'
+            break  
+        decNum += hexNumbers[char] * (16**exponent)
+        exponent -=1
+    return decNum
+
+print(hexToDec('ABC'))    
